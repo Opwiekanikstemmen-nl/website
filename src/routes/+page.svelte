a7421a1ff0f5a7842e6009984bece7a95b792f24
Floris Jansen <florismartijnjansen@gmail.com>
2023-10-16 21:28:55 +0200

    remove self closing slash

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
 
 <svelte:head>
 	<title>Op wie kan ik stemmen?</title>
-	<meta name="description" content="Op wie kan in stemmen? TK2023" />
+	<meta name="description" content="Op wie kan in stemmen? TK2023">
 </svelte:head>
 
 <main>
