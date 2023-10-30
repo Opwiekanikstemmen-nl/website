d771af0e030094d06cab4e45af58ac9fdee9867a
Floris Jansen <florismartijnjansen@gmail.com>
2023-10-30 19:48:16 +0100

    add gemeente x kieskring pairing

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,10 +1,12 @@
 import kandidaten from '$lib/data/kandidaten.json';
 import partijen from '$lib/data/partijen.json';
+import kieskringen from '$lib/data/gemeente-kieskringen.json';
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
 	return {
 		kandidaten: kandidaten,
 		partijen: partijen,
+		kieskringen: kieskringen,
 	};
 }
