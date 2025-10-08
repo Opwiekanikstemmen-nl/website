5302787bf2b6b444ef388e3d672f619d63860970
Floris Jansen <florismartijnjansen@gmail.com>
2025-10-08 15:07:43 +0200

    update bronnen page with 2025 links

--- src/routes/bronnen/+page.svelte
+++ src/routes/bronnen/+page.svelte
@@ -11,7 +11,7 @@
 		<p>De data op onze website komt uit de volgende bronnen:</p>
 		<ul>
 			<li>
-				De <a href="https://www.kiesraad.nl/adviezen-en-publicaties/proces-verbalen/2023/10/23/definitief-vastgestelde-kandidatenlijsten-tk-2023">kandidatenlijsten</a> met naam, woonplaats en in een deel van de gevallen geslacht komen van de <a href="https://www.kiesraad.nl/">Kiesraad</a>.
+				De <a href="https://www.kiesraad.nl/adviezen-en-publicaties/publicaties/2025/09/25/overzicht-kandidatenlijsten-tweede-kamerverkiezing-2025">kandidatenlijsten</a> met naam, woonplaats en in een deel van de gevallen geslacht komen van de <a href="https://www.kiesraad.nl/">Kiesraad</a>.
 			</li>
 			<li>
 				Informatie over woonplaatsen en gemeenten komen van het <a href="https://www.kadaster.nl/">Kadaster</a> en <a href="https://cbs.nl/">CBS</a>. We gebruiken de <a href="https://data.labs.kadaster.nl/_api/datasets/cbs/wbk">Wijk- en Buurtkaart 2020</a>, <a href="https://bag.basisregistraties.overheid.nl">Basisregistratie Adressen en Gebouwen (BAG)</a> en <a href="https://opendata.cbs.nl/#/CBS/nl/dataset/84929NED/table">Gebieden in Nederland 2021</a>.
@@ -61,7 +61,7 @@
 				<a href="https://www.instagram.com/zoetst/">Zoë Sabajo</a> helpt ons met tips, feedback en plaatst nuttige informatie over de Tweede Kamerverkiezing op haar Instagram.
 			</li>
 			<li>
-				We staan op de <a href="https://verkiezingen.prodemos.nl/verkiezingsinformatie/tweede-kamerverkiezing/">verkiezings-informatie-pagina</a> van <a href="https://prodemos.nl/">ProDemos</a> (ook bekend van de <a href="https://tweedekamer2023.stemwijzer.nl/">Stemwijzer</a>).
+				We staan op de <a href="https://verkiezingen.prodemos.nl/verkiezingsinformatie/tweede-kamer/#partijen-en-kandidaten-bepalen-op-wie-je-gaat-stemmen">verkiezings-informatie-pagina</a> van <a href="https://prodemos.nl/">ProDemos</a> (ook bekend van de <a href="https://tweedekamer2025.stemwijzer.nl/">Stemwijzer</a>).
 			</li>
 		</ul>
 	</section>
